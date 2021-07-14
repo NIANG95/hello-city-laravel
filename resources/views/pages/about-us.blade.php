080475280715d08f5c7a24a2f411ddfc8019f850
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2021-07-14 13:15:26 +0100

    Add styling with tailwind Css

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -3,9 +3,15 @@
 @section('title', '  About Us | '.config('app.name'))
 
 @section('content')
-    <img src="{{ asset('images/info-logo.png') }}" alt="Info Logo" width="150" height="100">
-    <p>Built with &hearts; by Mame Sandeck NIANG</p>
-    <p><a href="{{ route('home') }}"><button class="btn info">Retour à la page d'accueil</button></a></p>
+    <img src="{{ asset('images/info-logo.png') }}" alt="Info Logo" width="200" height="100" class="my-12 rounded-full shadow-md ">
+
+    <h2 class=" mb-5 text-gray-700 ">
+        Built with <span class="text-pink-500">&hearts;</span> by Mame Sandeck NIANG
+    </h2>
+
+    <p>
+        <a href="{{ route('home') }}" class="text-indigo-500 hover:text-indigo-600 underline">Retour à la page d'accueil</a>
+    </p>
 @endsection
         
       
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,11 @@
 @extends('app')
 
-@section('content')
-    <img src="{{ asset('images/senegal.jpg') }}" alt="Senegal Flag" width="150" height="100">
-    <h1>hello from Dakar !</h1>
-    <p>It's currently {{ date('h:i A') }} </p>
+@section('content') 
+
+    <img src="{{ asset('images/senegal.jpg') }}" alt="Senegal Flag" width="200" height="100" class=" mt-12 rounded shadow-md h-32 ">
+
+    <h1 class="mt-5  text-3xl sm:text-5xl font-semibold text-indigo-600">hello from Dakar !</h1>
+
+    <p class="text-lg text-gray-800">It's currently {{ date('h:i A') }} </p>
 
 @endsection
