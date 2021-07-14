7a44e4fe3dfd6c71b6706f403cf587229d47fc8a
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2021-07-14 17:11:52 +0100

    make use Route::view instead of Route::get

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/', 'pages.home')->name('home');
+Route::view('/about-us', 'pages.about-us')->name('about');
 
-Route::get('/', function () {
-    return view('pages.home');
-})->name('home');
-
-Route::get('/about-us', function (){
-    return view('pages.about-us');
-})->name('about');
-
+// Route::get('/', fn() => view(('pages.home')))->name('home');
+// Route::get('/about-us', fn() => view(('pages.about-us')))->name('about');
