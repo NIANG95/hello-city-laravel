1bf9bbadbbfd21ca4d6035c22e30e0b56364a05c
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2021-07-14 11:52:12 +0100

    Add images

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -3,6 +3,7 @@
 @section('title', '  About Us | '.config('app.name'))
 
 @section('content')
+    <img src="images/info-logo.png" alt="Info Logo" width="150" height="100">
     <p>Built with &hearts; by Mame Sandeck NIANG</p>
     <p><a href="{{ route('home') }}"><button class="btn info">Retour à la page d'accueil</button></a></p>
 @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 
 @section('content')
-
+    <img src="images/senegal.jpg" alt="Senegal Flag" width="150" height="100">
     <h1>hello from Dakar !</h1>
     <p>It's currently {{ date('h:i A') }} </p>
 
